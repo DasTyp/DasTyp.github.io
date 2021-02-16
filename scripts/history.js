e1b9d83425526aa665b78cc5c0e6d8bf2e81aa97
Christian <litkec.tin20@student.dhbw-heidenheim.de>
2021-02-16 11:59:14 +0100

    removed attempt at timeline, added gap to grid

--- scripts/history.js
+++
@@ -1,6 +0,0 @@
-window.onscroll = () => {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById("timeline").style.height = scrolled + "%";
-}
