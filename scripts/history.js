48ffb1fc8fc79368fae3f1eb13aa5ca21d3eb004
Christian <litkec.tin20@student.dhbw-heidenheim.de>
2021-02-16 08:41:55 +0100

    Added first part of side timeline, added some formating which still needs fixing

---
+++ scripts/history.js
@@ -0,0 +1,6 @@
+window.onscroll = () => {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("timeline").style.height = scrolled + "%";
+}
