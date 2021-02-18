3f85ed5d40fd15a84e349144aa88a2938c504557
Knittel <knittet@emea.corpdir.net>
2021-02-18 08:58:29 +0100

    Changed javascript animation type

--- scripts/script.js
+++ scripts/script.js
@@ -10,72 +10,26 @@ let openShopping = document.getElementById('open-cart');
 let shop_open = false;
 
 menuButton.onclick = () => {
-    let left = 100;
-    sidebar.style.left = "100%";
-    let slider = setInterval(() => {
-        if(left <= 0) {
-            clearInterval(slider);
-        } else {
-            left -= 10;
-            sidebar.style.left = left + "%";
-        }
-    }, 8);
+    sidebar.style.width = "100%";
 }
 
 closeSidebar.onclick = () => {
-    let left = 0;
-    sidebar.style.left = "0%";
-    let slider = setInterval(() => {
-        if(left >= 100) {
-            clearInterval(slider);
-        } else {
-            left += 10;
-            sidebar.style.left = left + "%";
-        }
-    }, 8);
+    sidebar.style.width = "0";
 }
 
 openShopping.onclick = () => {
     if(!shop_open) {
+        if(window.innerWidth < 400) {
+            shoppingCart.style.width = "100%";
+        } else {
+            shoppingCart.style.width = "350px";
+        }
         shop_open = true;
-        let size = shoppingCart.clientWidth;
-        let right = -size;
-        let slider = setInterval(() => {
-            if(right >= 0) {
-                right = 0;
-                shoppingCart.style.right = right + "px";
-                clearInterval(slider);
-            } else if(!shop_open) {
-                clearInterval(slider);
-            }else {
-                if(right + size/10 > 0 && right + size/10 < size/10) {
-                    right = 0;
-                }
-                right += size/10;
-                shoppingCart.style.right = right + "px";
-            }
-        }, 8);
     } else {
+        shoppingCart.style.width = "0";
         shop_open = false;
-        let size = shoppingCart.clientWidth;
-        let right = parseInt(shoppingCart.style.right);
-        let slider = setInterval(() => {
-            if(right <= -size) {
-                right = -size;
-                shoppingCart.style.right = right + "px";
-                clearInterval(slider)
-            } else if(shop_open) {
-                clearInterval(slider)
-            } else {
-                if(right - size/10 < -size && right - size/10 > -size + size/10) {
-                    right = -size;
-                }
-                right -= size/10;
-                shoppingCart.style.right = right + "px";
-            }
-        }, 8);
     }
-} 
+}  
 
 age_confirm.onclick = () => {
     age_blocker.classList.add("clicked")
